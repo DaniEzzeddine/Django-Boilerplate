bc92a70daafc29ddf03868be3d3f6b2653c54f0c
DaniEzzeddine <dani.izzedin@gmail.com>
2019-09-06 16:46:08 -0700

    boilerplate

---
+++ src/django_boilerplate/settings/production.py
@@ -0,0 +1,32 @@
+from .base import *
+
+DEBUG = config('DEBUG', cast=bool)
+
+ALLOWED_HOSTS = ['id-address', 'www.???.com']
+
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        "PASSWORD": config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': '',
+    }
+}
